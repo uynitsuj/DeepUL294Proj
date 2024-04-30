ea81a9bdf3f20ae1444503927c0767b212e8e074
sandeep <sandeep.m@berkeley.edu>
2024-04-30 21:18:59 +0000

    mae model

--- DUL294P/train_gen.py
+++ DUL294P/train_gen.py
@@ -3,161 +3,255 @@
 
 import argparse
 import json
+
 # import pickle
 import pprint
 
 import torch
 from torch.utils import data
-from torchvision import transforms
+from torchvision.transforms import v2 as T
+from torch import nn
+from torch.nn import functional as F
 from torchvision.transforms.functional import pil_to_tensor
-from tqdm import trange, tqdm
+from tqdm import tqdm
 from datasets import load_dataset
 import os
-import matplotlib.pyplot as plt
-import PIL.Image
-from DUL294P.model_transformer import FoveatedTransformer
-from DUL294P.foveation.foveate_image import FoveateImage
+import logging
+import sys
+torch.multiprocessing.set_sharing_strategy('file_system')
+sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), ".."))
+
+
+# from DUL294P.model_transformer import FoveatedTransformer
+# from DUL294P.foveation.foveate_image import FoveateImage
 from DUL294P.encoders.openclip_encoder import *
+from DUL294P.mae.models_mae import MaskedAutoencoderViT, partial
 import time
-from datasets.utils.file_utils import get_datasets_user_agent
+# from datasets.utils.file_utils import get_datasets_user_agent
+
 
 def main():
     p = argparse.ArgumentParser(description=__doc__)
-    p.add_argument('--model_config', type=str,
-                   help='the model config file', default='configs/model1.json')
-    p.add_argument('--training_config', type=str,
-                   help='the training config file', default='configs/train1.json')
-    p.add_argument('--resume', type=str,
-                   help='the checkpoint to resume from')
-    p.add_argument('--debug', action='store_true', default=False)
+    p.add_argument(
+        "--model_config",
+        type=str,
+        help="the model config file",
+        default="configs/mae_model.json",
+    )
+    p.add_argument(
+        "--training_config",
+        type=str,
+        help="the training config file",
+        default="configs/train1.json",
+    )
+    p.add_argument("--device_id", type=int, default=0)
+    p.add_argument("--resume", type=str, help="the checkpoint to resume from")
+    p.add_argument("--debug", action="store_true", default=False)
     args = p.parse_args()
 
     dir = os.path.dirname(os.path.abspath(__file__))
+    
+    log = logging.getLogger()
 
     ### Setup model and train config ###
-    print('Checking for model config path: ', os.path.exists(os.path.join(dir, args.model_config)))
-    print('Checking for train config path: ', os.path.exists(os.path.join(dir, args.training_config)))
+    print(
+        "Checking for model config path: ",
+        os.path.exists(os.path.join(dir, args.model_config)),
+    )
+    print(
+        "Checking for train config path: ",
+        os.path.exists(os.path.join(dir, args.training_config)),
+    )
     model_config = json.load(open(os.path.join(dir, args.model_config)))
     training_config = json.load(open(os.path.join(dir, args.training_config)))
-    opt_config = training_config['optimizer']
-    wandb_config = training_config['wandb']
-    print('Model config:')
+    opt_config = training_config["optimizer"]
+    wandb_config = training_config["wandb"]
+    print("Model config:")
     pprint.pprint(model_config)
-    print('\nTraining config:')
+    print("\nTraining config:")
     pprint.pprint(training_config)
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    batch_size = training_config['batch_size_per_device']
+    device = torch.device(f"cuda:{args.device_id}" if torch.cuda.is_available() else "cpu")
+    batch_size = training_config["batch_size_per_device"]
 
     ds_name = "HuggingFaceM4/COCO"
     dataset = load_dataset(ds_name)
 
-    if wandb_config['use_wandb'] == 1:
-        print('Logging on wandb')
-        import wandb
-        wandb.init(project=wandb_config['project'],
-                   config={'model': model_config,
-                           'training': training_config,
-                           'dataset name': ds_name},
-                   save_code=True)
-        
-    model = FoveatedTransformer(k = model_config["k"], out_channels=5, dropout=model_config["dropout"], num_layers=model_config["num_layers"], channels = model_config["channels"], num_heads=model_config["num_heads"], ratio=model_config["ratio"])
+    # if wandb_config["use_wandb"] == 1:
+    #     print("Logging on wandb")
+    #     import wandb
+
+    #     wandb.init(
+    #         project=wandb_config["project"],
+    #         config={
+    #             "model": model_config,
+    #             "training": training_config,
+    #             "dataset name": ds_name,
+    #         },
+    #         save_code=True,
+    #     )
+
+    if model_config["model"] == "MAE":
+        model = MaskedAutoencoderViT(
+            patch_size=model_config["patch_size"],
+            embed_dim=model_config["embed_dim"],
+            depth=model_config["depth"],
+            num_heads=model_config["num_heads"],
+            decoder_embed_dim=model_config["decoder_embed_dim"],
+            decoder_depth=model_config["decoder_depth"],
+            decoder_num_heads=model_config["decoder_num_heads"],
+            mlp_ratio=model_config["mlp_ratio"],
+            out_chans=model_config["out_chans"],
+            norm_layer=partial(nn.LayerNorm, eps=1e-6),
+            return_pooled=True,
+        )
+        model.load_state_dict(torch.load(model_config['pretrained_path'])["model"], strict=False)
+    else:
+        raise ValueError("Model not supported")
+        # model = FoveatedTransformer(
+        #     k=model_config["k"],
+        #     out_channels=5,
+        #     dropout=model_config["dropout"],
+        #     num_layers=model_config["num_layers"],
+        #     channels=model_config["channels"],
+        #     num_heads=model_config["num_heads"],
+        #     ratio=model_config["ratio"],
+        # )
+
     if args.resume:
         model = torch.load(args.resume)
     else:
-        if os.path.exists('checkpoints/model.pth'):
-            model.load_state_dict(torch.load('checkpoints/model.pth'))
-            print('Loaded pretrained model')
+        if os.path.exists("checkpoints/model.pth"):
+            model.load_state_dict(torch.load("checkpoints/model.pth"))
+            print("Loaded pretrained model")
         else:
-            print('Training from scratch')
-            
-    clipencoder = OpenCLIPNetworkConfig(device=device).setup()
+            print("Training from scratch")
 
-    optimizer = torch.optim.Adam(model.parameters(), lr=opt_config['lr'])
-    scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, training_config['epochs'])
-    
+    clipencoder = OpenCLIPNetworkConfig(device=device).setup().eval().to(device).float()
+    model.to(device)
+    model.float()
+
+    optimizer = torch.optim.Adam(model.parameters(), lr=opt_config["lr"])
+    num_iter = 500
+    scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(
+        optimizer, training_config["epochs"]*num_iter, eta_min=1e-7
+    )
+
     ### Dataloaders ###
-    transform = transforms.Compose([
-        torchvision.transforms.Normalize(
-                    mean=[0.48145466, 0.4578275, 0.40821073],
-                    std=[0.26862954, 0.26130258, 0.27577711],
-                ),
-    ])
-    dataset.set_format(type='torch')
+    dataset.set_format(type="torch")
 
     def collate_fn(batch):
+        
+        images = []
+        for ex in batch:
+            img = ex['image']
+            if img.size(0) == 1:
+                img = img.repeat(3, 1, 1)
+            images.append(img / 255.)
+        
         return {
-            'image': [(x['image'].permute(2,0,1)/255.0) for x in batch],
-            # 'clip_image': torch.stack([clipencoder.preprocess(x['image']) for x in batch]),
-            'sentences': [x['sentences'] for x in batch]
+            "image": images,
+            'clip_image': torch.stack([clipencoder.process(x) for x in images]),
+            "sentences": [x["sentences"] for x in batch],
         }
-    trainloader = data.DataLoader(dataset["train"], batch_size, drop_last=True,
-                            num_workers=0,
-                            shuffle=True,
-                            collate_fn=collate_fn)
-    
-    testloader = data.DataLoader(dataset["validation"], batch_size, drop_last=True,
-                            num_workers=0,
-                            collate_fn=collate_fn)
-    
+
+    trainloader = data.DataLoader(
+        dataset["train"],
+        batch_size,
+        drop_last=True,
+        num_workers=5,
+        shuffle=True,
+        collate_fn=collate_fn,
+    )
+
+    valloader = data.DataLoader(
+        dataset["validation"],
+        batch_size,
+        drop_last=False,
+        num_workers=5,
+        collate_fn=collate_fn,
+    )
+
     def train_one_epoch():
         model.train()
-        for i, batch in enumerate(tqdm(trainloader)):
-            for image, sentences in zip(batch['image'], batch['sentences']):
-                clipimg = clipencoder.process(image).unsqueeze(0).half().cuda()
-                imgenc = clipencoder.model.encode_image(clipimg).float()
-                # import pdb; pdb.set_trace()
-                c, h, w = image.size()
-                print("Original Image Size", w, h)
-                print("Pixel Count", w*h)
-                img = image.permute((1,2,0)) # [H, W, C]
-                fimg = FoveateImage(w, h)
-                # start = time.time()
-                foveatedimg, idxs, rs, thetas = fimg.foveate(img)
-                # elapsed = (time.time() - start)
-                # print(f"Elapsed time: {elapsed}(s)")
-                # print(f"Frequency: {1/elapsed}(fps)")
-                # print("Foveated Pixel Count", foveatedimg.shape)
+        pbar = tqdm(enumerate(trainloader), total=num_iter)
+        for i, (batch) in pbar:
+            img = batch['clip_image']
+            with torch.no_grad():
+                clipimg = clipencoder.process(img).float().to(device)
+                pooled_clip, pred_clip = clipencoder.model.encode_image(clipimg)
+            # import pdb; pdb.set_trace()
+            start = time.time()
+            latent, mask, ids_restore = model.forward_encoder(clipimg, training_config['masking_prob'])
+            pred_mae, pooled_mae = model.forward_decoder(latent, ids_restore)
+            elapsed = time.time() - start
+            # print(f"Elapsed time: {elapsed}(s)")
+            # print(f"Frequency: {1/elapsed}(fps)")
+            mask = mask.bool()
+            optimizer.zero_grad()
+            # check for NaNs
+            if torch.isnan(pred_mae).any():
+                print("NaN in prediction")
+                raise ValueError("NaN in prediction")
 
-                # recon = torch.zeros((h,w,3), dtype=foveatedimg.dtype)
-                # recon.view(-1, 3)[idxs] = foveatedimg[range(len(foveatedimg))]
-
-                # print(f"Compression: {len(idxs)/(w*h)*100}%")
-                # plt.title(sentences['raw'])
-                # plt.imshow(recon)
-                # plt.show()
-
-                # x = torch.linspace(0, h - 1, h)
-                # y = torch.linspace(0, w - 1, w)
-                # x, y = torch.meshgrid(x, y)
-                # x_centered = x - h/2
-                # y_centered = y - w/2
-                # imgspacex = x_centered.view(-1)[idxs]
-                # # imgspacey = torch.transpose(y_centered, 0,1).view(-1)[idxs]
-                # import pdb; pdb.set_trace()
+            if torch.isnan(pooled_mae).any():
+                print("NaN in pooled")
+                raise ValueError("NaN in pooled")
+            
+            if torch.isnan(pred_clip).any():
+                print("NaN in clip prediction")
+                raise ValueError("NaN in clip prediction")
+            
+                
+            loss = torch.nn.MSELoss()(pred_mae[mask], pred_clip[mask])
+            loss += torch.nn.MSELoss()(pooled_mae, pooled_clip) * training_config["pooled_loss_weight"]
+            loss.backward()
+            optimizer.step()
+            scheduler.step()
+            pbar.set_description(f"Loss: {loss.item()}, Time: {elapsed}(s), Frequency: {1/elapsed}(fps)")
+            if i > num_iter:
+                break
+    
+    @torch.no_grad()
+    def val_one_epoch(loader):
+        model.eval()
+        losses = []
+        times = []
+        clip_sims = []
+        pbar = tqdm(enumerate(loader), total=len(loader))
+        for i, batch in pbar:
+            for image, sentences in zip(batch["image"], batch["sentences"]):
+                clipimg = clipencoder.process(image).unsqueeze(0).float().to(device)
+                pooled_clip, pred_clip = clipencoder.model.encode_image(clipimg)
+                clip_sent = clipencoder.model.encode_text(clipencoder.tokenizer(sentences['raw']).to(device), normalize=True)
                 start = time.time()
-                fovenc = model(foveatedimg, rs, thetas)
-                elapsed = (time.time() - start)
-                print(f"Elapsed time: {elapsed}(s)")
-                print(f"Frequency: {1/elapsed}(fps)")
-                optimizer.zero_grad()
-                loss = torch.nn.MSELoss()(fovenc, imgenc)
-                loss.backward()
-                optimizer.step()
-                scheduler.step()
-
-
+                latent, mask, ids_restore = model.forward_encoder(clipimg, training_config['masking_prob'])
+                pred_mae, pooled_mae = model.forward_decoder(latent, ids_restore)
+                elapsed = time.time() - start
+                mask = mask.bool()
+                loss = torch.nn.MSELoss()(pred_mae[mask], pred_clip[mask])
+                loss += torch.nn.MSELoss()(pooled_mae, pooled_clip) * training_config["pooled_loss_weight"]
+                clip_sim = F.cosine_similarity(pooled_mae, clip_sent).item()
+                # print(clip_sim.shape, clipimg.shape, clip_sent.shape)
+                losses.append(loss.item())
+                times.append(elapsed)
+                clip_sims.append(clip_sim)
+                pbar.set_description(f"Loss: {loss.item():4f}, Time: {elapsed:4f}(s), Frequency: {(1/elapsed):4f}(fps), Text Sim: {clip_sim:4f}")
+        
+        print(f"Mean Loss: {sum(losses) / len(losses)}")
+        print(f"Mean Time: {sum(times) / len(times)}")
+        print(f"Mean Clip Similarity: {sum(clip_sims) / len(clip_sims)}")
+        return sum(losses) / len(losses), sum(times) / len(times), sum(clip_sims) / len(clip_sims)
+    
 
     epoch = 0
-    try:
-        while True:
-            tqdm.write(f'Epoch {epoch}')
-            train_one_epoch()
-            epoch += 1
-            tqdm.write('')
-            torch.save(model.state_dict(), f'checkpoints/model.pth')
-    except KeyboardInterrupt:
-        pass
+    for _ in range(training_config["epochs"]):
+        tqdm.write(f"Epoch {epoch}")
+        train_one_epoch()
+        loss, val_time, clip_sims = val_one_epoch(valloader)
+        epoch += 1
+        tqdm.write("")
+        torch.save(model.state_dict(), f"checkpoints/model.pth")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
